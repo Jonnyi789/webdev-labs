1aecc30be04efdfd8dd054a06aa49cae0bd36b1b
Jonnyi789 <johnny080104@gmail.com>
2022-05-19 00:31:08 -0500

    self learn more just recording them

--- lab06/02-theme-switcher/index.js
+++ lab06/02-theme-switcher/index.js
@@ -5,3 +5,21 @@
     2. Modify the className property of the body tag 
        based on the button that was clicked.
 */
+const page = document.querySelector("body");
+const makeDesert = () => {
+   page.className = "desert";
+};
+const makeOcean = () => {
+   page.className = "ocean";
+};
+const makeContrast = () => {
+   page.className = "high-contrast";
+};
+const makeDefault = () => {
+   page.removeAttribute("class");
+};
+
+document.querySelector("#default").addEventListener('click', makeDefault);
+document.querySelector("#desert").addEventListener('click', makeDesert);
+document.querySelector("#ocean").addEventListener('click', makeOcean);
+document.querySelector("#high-contrast").addEventListener('click', makeContrast);
